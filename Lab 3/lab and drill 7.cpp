5ae213d1ba71ffec56c8461b99e9bb36cde19ba3
urkhani101-max <urkhani101@gmail.com>
2026-03-11 00:36:22 +0500

    Add files via upload

---
+++ Lab 3/lab and drill 7.cpp
@@ -0,0 +1,16 @@
+// Program for linear equation y = 2x + 3
+#include <iostream>
+using namespace std;
+
+int main() {
+    float x, y;
+
+    cout << "Enter value of x: ";
+    cin >> x;
+
+    y = 2*x + 3;
+
+    cout << "Value of y: " << y << endl;
+
+    return 0;
+}
