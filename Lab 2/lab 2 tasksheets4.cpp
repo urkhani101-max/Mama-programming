909a86af71e5d4967f0db5c93f8a3425138b8b6f
urkhani101-max <urkhani101@gmail.com>
2026-03-11 00:08:18 +0500

    Add files via upload

---
+++ Lab 2/lab 2 tasksheets4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Dear user kindly enter your age: ";//this is shown on screen 
+    cin >> a;//it is asking user to enter age in number like 22 etc
+    cout << a;
+
+    return 0;//program ends here
+}
