1ed917f5c5033492fd4f3dbb9ce61987762a5828
urkhani101-max <urkhani101@gmail.com>
2026-03-11 00:06:58 +0500

    Add files via upload

---
+++ Lab 2/lab 2 tasksheets 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;
+
+    cout << "a. Enter your name" << endl;
+    cout << "b. Enter your registration number" << endl;
+    cout << "c. Enter your CGPA" << endl;
+    cout << "d. Enter your pin" << endl;
+    cout << "e. Enter your password" << endl;
+    cout << "f. Dear customer the service will be not available today. Sorry for inconvenience created" << endl;
+    cout << "g. Happy to help!" << endl;
+    cout << "h. Great question!" << endl;
+    cout << "i. Nice to meet you!" << endl;
+    cout << "j. May I ask why that is?" << endl;
+    cout << "k. Thanks for bringing this to our attention!" << endl;
+    cout << "l. I completely understand why you'd want that." << endl;
+
+    return 0;
+}
