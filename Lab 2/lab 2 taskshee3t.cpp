27e5f13719607d02df71d36470f530397a80c7d9
urkhani101-max <urkhani101@gmail.com>
2026-03-11 00:07:42 +0500

    Add files via upload

---
+++ Lab 2/lab 2 taskshee3t.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 25;      cout << a << endl;   // 1) integer value
+    int b = 3.7;     cout << b << endl;   // 2) fraction in int (decimal cut)
+    float c = 3.7;   cout << c << endl;   // 3) fraction in float
+    float d = 8;     cout << d << endl;   // 4) integer in float
+
+    return 0;
+}
