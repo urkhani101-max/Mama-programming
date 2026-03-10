1d37c86141bc734134f41cefcfaa924723f90bbb
urkhani101-max <urkhani101@gmail.com>
2026-03-11 00:09:00 +0500

    Add files via upload

---
+++ Lab 2/lab 2tasksheets5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Print your name 10 times
+    cout << "Afnan Wazir" << endl;
+    cout << "Afnan Wazir" << endl;
+    cout << "Afnan Wazir" << endl;
+    cout << "Afnan Wazir" << endl;
+    cout << "Afnan Wazir" << endl;
+    cout << "Afnan Wazir" << endl;
+    cout << "Afnan Wazir" << endl;
+    cout << "Afnan Wazir" << endl;
+    cout << "Afnan Wazir" << endl;
+    cout << "Afnan Wazir" << endl;
+
+    // Print Pakistan 5 times
+    cout << "Pakistan" << endl;
+    cout << "Pakistan" << endl;
+    cout << "Pakistan" << endl;
+    cout << "Pakistan" << endl;
+    cout << "Pakistan" << endl;
+
+    // Print C++ 7 times
+    cout << "C++" << endl;
+    cout << "C++" << endl;
+    cout << "C++" << endl;
+    cout << "C++" << endl;
+    cout << "C++" << endl;
+    cout << "C++" << endl;
+    cout << "C++" << endl;
+
+    // Print first 5 numbers
+    cout << 1 << endl;
+    cout << 2 << endl;
+    cout << 3 << endl;
+    cout << 4 << endl;
+    cout << 5 << endl;
+
+    // Print X, Y and Z
+    cout << "X" << endl;
+    cout << "Y" << endl;
+    cout << "Z" << endl;
+
+    return 0;
+}
